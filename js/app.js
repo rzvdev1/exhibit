c5f98a72f0ba1c78e8f5fff8c0ba61993d9a82f6
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-02-09 13:24:11 -0800

    new changes

--- js/app.js
+++ js/app.js
@@ -1,91 +1,91 @@
-"use strict";
-//Greeting User
-let firstName = prompt("Hello, What is your first name?");
-alert(
-  "Thank you for visiting my page and Welcome " +
-    firstName.charAt(0).toUpperCase() +
-    firstName.substring(1) +
-    "!"
-);
-console.log(firstName.charAt(0).toUpperCase() + firstName.substring(1));
+// "use strict";
+// //Greeting User
+// let firstName = prompt("Hello, What is your first name?");
+// alert(
+//   "Thank you for visiting my page and Welcome " +
+//     firstName.charAt(0).toUpperCase() +
+//     firstName.substring(1) +
+//     "!"
+// );
+// console.log(firstName.charAt(0).toUpperCase() + firstName.substring(1));
 
-//Question 1
-let website = prompt(
-  "Do you want a website to launch your dream business, Yes or No?"
-);
-if (website === "yes") {
-  alert(
-    website.charAt(0).toUpperCase() +
-      website.substring(1) +
-      ", Awesome looking foward working with you!"
-  );
-} else {
-  alert(website.charAt(0).toUpperCase() + website.substring(1) + ", Why not?");
-}
-console.log(website.charAt(0).toUpperCase() + website.substring(1));
+// //Question 1
+// let website = prompt(
+//   "Do you want a website to launch your dream business, Yes or No?"
+// );
+// if (website === "yes") {
+//   alert(
+//     website.charAt(0).toUpperCase() +
+//       website.substring(1) +
+//       ", Awesome looking foward working with you!"
+//   );
+// } else {
+//   alert(website.charAt(0).toUpperCase() + website.substring(1) + ", Why not?");
+// }
+// console.log(website.charAt(0).toUpperCase() + website.substring(1));
 
-//Question 2
-let music = prompt("Do you listen to Lo-fi, Yes or No?");
-if (music === "yes") {
-  alert(
-    music.charAt(0).toUpperCase() +
-      music.substring(1) +
-      ", thats good and same here it helps me to concentrate!"
-  );
-} else {
-  alert(
-    music.charAt(0).toUpperCase() +
-      music.substring(1) +
-      ", you should try out it helps to reduce stress and enhance focus."
-  );
-}
-console.log(music.charAt(0).toUpperCase() + music.substring(1));
+// //Question 2
+// let music = prompt("Do you listen to Lo-fi, Yes or No?");
+// if (music === "yes") {
+//   alert(
+//     music.charAt(0).toUpperCase() +
+//       music.substring(1) +
+//       ", thats good and same here it helps me to concentrate!"
+//   );
+// } else {
+//   alert(
+//     music.charAt(0).toUpperCase() +
+//       music.substring(1) +
+//       ", you should try out it helps to reduce stress and enhance focus."
+//   );
+// }
+// console.log(music.charAt(0).toUpperCase() + music.substring(1));
 
-//Question 3
-let drink = prompt("Do you drink coffee, Yes or No?");
-if (drink === "yes") {
-  alert(
-    drink.charAt(0).toUpperCase() +
-      drink.substring(1) +
-      ", glad you do because coffee has unique benefits."
-  );
-} else {
-  alert(
-    drink.charAt(0).toUpperCase() +
-      drink.substring(1) +
-      ", try it out sometime coffee has unique benefits."
-  );
-}
-console.log(drink.charAt(0).toUpperCase() + drink.substring(1));
+// //Question 3
+// let drink = prompt("Do you drink coffee, Yes or No?");
+// if (drink === "yes") {
+//   alert(
+//     drink.charAt(0).toUpperCase() +
+//       drink.substring(1) +
+//       ", glad you do because coffee has unique benefits."
+//   );
+// } else {
+//   alert(
+//     drink.charAt(0).toUpperCase() +
+//       drink.substring(1) +
+//       ", try it out sometime coffee has unique benefits."
+//   );
+// }
+// console.log(drink.charAt(0).toUpperCase() + drink.substring(1));
 
-//Question 4
-let veteran = prompt("Are you a Veteran, Yes or No?");
-if (veteran === "yes") {
-  alert(
-    veteran.charAt(0).toUpperCase() +
-      veteran.substring(1) +
-      ", aye! Im a veteran too. I offer a 25% discount to build your website."
-  );
-} else {
-  alert(
-    veteran.charAt(0).toUpperCase() +
-      veteran.substring(1) +
-      ", that's ok and if you anyone that wants a veteran disount to build a webiste have them reach out to me"
-  );
-}
-console.log(veteran.charAt(0).toUpperCase() + veteran.substring(1));
+// //Question 4
+// let veteran = prompt("Are you a Veteran, Yes or No?");
+// if (veteran === "yes") {
+//   alert(
+//     veteran.charAt(0).toUpperCase() +
+//       veteran.substring(1) +
+//       ", aye! Im a veteran too. I offer a 25% discount to build your website."
+//   );
+// } else {
+//   alert(
+//     veteran.charAt(0).toUpperCase() +
+//       veteran.substring(1) +
+//       ", that's ok and if you anyone that wants a veteran disount to build a webiste have them reach out to me"
+//   );
+// }
+// console.log(veteran.charAt(0).toUpperCase() + veteran.substring(1));
 
-//Question 5
-let pets = prompt("Do you have pets, Yes or No?");
-if (pets === "yes") {
-  alert(
-    pets.charAt(0).toUpperCase() + pets.substring(1) + ", OMG! I have a Husky."
-  );
-} else {
-  alert(
-    pets.charAt(0).toUpperCase() +
-      pets.substring(1) +
-      ", they make good companions."
-  );
-}
-console.log(pets.charAt(0).toUpperCase() + pets.substring(1));
+// //Question 5
+// let pets = prompt("Do you have pets, Yes or No?");
+// if (pets === "yes") {
+//   alert(
+//     pets.charAt(0).toUpperCase() + pets.substring(1) + ", OMG! I have a Husky."
+//   );
+// } else {
+//   alert(
+//     pets.charAt(0).toUpperCase() +
+//       pets.substring(1) +
+//       ", they make good companions."
+//   );
+// }
+// console.log(pets.charAt(0).toUpperCase() + pets.substring(1));
